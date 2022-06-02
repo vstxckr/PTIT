9bffa33b62f289fc1681fb6be62e46444ebd3b25
ManhDX8 <manhdaoxuan2808@gmail.com>
2022-06-02 17:52:48 +0700

    done!

--- THCS2/12_C01015_PHUONGTRINHBACHAI.c
+++ THCS2/12_C01015_PHUONGTRINHBACHAI.c
@@ -1,37 +1,19 @@
 #include<stdio.h>
 #include<math.h>
-
 int main()
 {
-	double a,b,c,x,x1,x2,delta;
-	
-	scanf("%lf %lf %lf", &a, &b, &c);
-	
-	
-		if(a==0){
-			if(b==0){
-				if(c!=0){
-					printf("NO");
-				}
-			}else{
-				x=-c/b;
-				printf("%.2lf", x);
-			}
-		}else{
-			delta=b*b-4*a*c;
-			if(delta<0){
-				printf("NO");
-			}else{
-				if(delta==0){
-					x=-b/(2*a);
-					printf("%.2lf", x);
-				}else{
-					x1=((-b+sqrt(delta))/(2*a));
-					x2=((-b-sqrt(delta))/(2*a));
-					printf("%.2lf %.2lf", x1, x2);
-				}
-			}
-		}
-	
-	return 0;
+    double a,b,c;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    double delta = b*b-4*a*c;
+    if ((int)delta<0)
+    {
+        printf("NO");
+        return 0;
+    }
+    if ((int)delta==0)
+    {
+        printf("%.2lf",(double)(-b-sqrt(delta))/(2*a));
+        return 0;
+    }
+    printf("%.2lf %.2lf",(double)(-b+sqrt(delta))/(2*a),(double)(-b-sqrt(delta))/(2*a));
 }
